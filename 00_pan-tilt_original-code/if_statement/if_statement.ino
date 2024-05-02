8677b8f0c1d4957c2e55ee5c838b12637a160476
Tesalonika R. M. Wibisomo <122904327+tesawibisono@users.noreply.github.com>
2024-05-02 15:09:13 +0100

    Update if_statement.ino
    change float to volatile float

--- 00_pan-tilt_original-code/if_statement/if_statement.ino
+++ 00_pan-tilt_original-code/if_statement/if_statement.ino
@@ -17,8 +17,7 @@ Servo servo_9; // Declare servo object for motor 9
 Servo servo_10; // Declare servo object for motor 10
 DFRobot_HuskyLens huskylens; // Declare HuskyLens object
 
-float mind_n_currentx = 90; // Initialize current x position
-float mind_n_currenty = 90; // Initialize current y position
+volatile float mind_n_currentx, mind_n_currenty;
 
 void setup() {
     // Initialize serial communication for debugging
@@ -55,24 +54,14 @@ void loop() {
         servo_9.write(mind_n_currentx);
     }
 
-    if (yCenter > 150) {
-        mind_n_currenty += 1.0;
-        mind_n_currenty = constrain(mind_n_currenty, 0, 180);
-        servo_10.write(mind_n_currenty);
-    }
-    else if (yCenter > 10 && yCenter < 90) {
-        mind_n_currenty -= 1;
-        mind_n_currenty = constrain(mind_n_currenty, 0, 180);
-        servo_10.write(mind_n_currenty);
-
-    // Print object position (for debugging)
-    Serial.print("Object Position - X: ");
-    Serial.print(objectX);
-    Serial.print(", Y: ");
-    Serial.println(objectY);
-    Serial.print("Servo angles - Pan: ");
-    Serial.print(panAngle);
-    Serial.print(", Tilt: ");
-    Serial.println(tiltAngle);
-    }
+   // if (yCenter > 150) {
+   //     mind_n_currenty += 1.0;
+   //     mind_n_currenty = constrain(mind_n_currenty, 0, 180);
+   //     servo_10.write(mind_n_currenty);
+   // }
+   // else if (yCenter > 10 && yCenter < 90) {
+   //     mind_n_currenty -= 1;
+   //     mind_n_currenty = constrain(mind_n_currenty, 0, 180);
+   //     servo_10.write(mind_n_currenty);
+   // }
 }
