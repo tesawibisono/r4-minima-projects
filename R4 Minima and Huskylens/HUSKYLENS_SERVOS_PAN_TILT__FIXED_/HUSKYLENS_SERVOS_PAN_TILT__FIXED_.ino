9041c8b5c9e67d0ff1f4fbe30d97f739e3e62413
Tesalonika R. M. Wibisomo <122904327+tesawibisono@users.noreply.github.com>
2024-05-03 14:42:47 +0100

    Code update for detached camera

--- R4 Minima and Huskylens/HUSKYLENS_SERVOS_PAN_TILT__FIXED_/HUSKYLENS_SERVOS_PAN_TILT__FIXED_.ino
+++ R4 Minima and Huskylens/HUSKYLENS_SERVOS_PAN_TILT__FIXED_/HUSKYLENS_SERVOS_PAN_TILT__FIXED_.ino
@@ -1,6 +1,6 @@
 /* The HuskyLens module communicates using the I2C protocol,
  * so you should connect it to the Arduino's dedicated I2C pins.
- * On Arduino R4 Minima boards, these pins are labeled as SDA (data line)
+ * On most Arduino boards, these pins are labeled as SDA (data line)
  * and SCL (clock line). 
  * 
  * The pins used for I2C on the UNO R4 Minima are the following:
@@ -13,53 +13,31 @@
 
 // Pin Definitions
 #define PAN_SERVO_PIN 9 //base
-//** #define TILT_SERVO_PIN 10 //top
+#define TILT_SERVO_PIN 10 //top
 
 // Global Variables
 Servo panServo;
-//** Servo tiltServo;
+Servo tiltServo;
 DFRobot_HuskyLens huskyLens;
-
-// Servo movement increment and delay
- const int SERVO_INCREMENT = 1; // Adjust as needed for smoother movement
- const int SERVO_DELAY = 15; // Delay in milliseconds between servo movements
-
 
 void setup() {
   // Initialize serial communication for debugging
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Attach servos to pins
   panServo.attach(PAN_SERVO_PIN);
-  //** tiltServo.attach(TILT_SERVO_PIN);
+  tiltServo.attach(TILT_SERVO_PIN);
 
   // Initialize HuskyLens
   huskyLens.beginI2CUntilSuccess();
   huskyLens.writeAlgorithm(ALGORITHM_OBJECT_TRACKING); // Set algorithm to object tracking mode
   
   // Set initial position of servos
-  panServo.write(90); // Center position
-  //** tiltServo.write(90); // Center position
+  panServo.write(30); // Center position
+  tiltServo.write(90); // Center position
 
   delay(2000);
 }
-
-// Function to smoothly move the servo to the target angle
- void moveServoSmoothly(Servo& servo, int targetAngle) {
-  int currentAngle = servo.read();
-  while (currentAngle != targetAngle) {
-    if (currentAngle < targetAngle) {
-      currentAngle += SERVO_INCREMENT;
-      if (currentAngle > targetAngle) currentAngle = targetAngle;
-    } else {
-      currentAngle -= SERVO_INCREMENT;
-     if (currentAngle < targetAngle) currentAngle = targetAngle;
-    }
-    servo.write(currentAngle);
-    delay(SERVO_DELAY);
-  }
- } 
-
 
 void loop() {
   // Request data from HuskyLens
@@ -69,34 +47,19 @@ void loop() {
   int objectX = huskyLens.readBlockParameter(1).xCenter;
   int objectY = huskyLens.readBlockParameter(1).yCenter;
 
-// Check if object is detected
-  if (objectX == -1 && objectY == -1) {
-    // Object not detected, move servos to initial position
-    panServo.write(90); // Center position
-    //** tiltServo.write(90); // Center position
-
-    // Print servo angles (for debugging)
-    //** Serial.println("Servo angles - Pan: 90, Tilt: 90 (Object not detected)");
-  } 
-  else {
   // Map object position to servo angles
-  int panAngle = map(objectX, 0, 320, 180, 0); 
-  //** int tiltAngle = map(objectY, 0, 240, 0, 180);
+  int panAngle = map(objectX, 0, 320, 0, 180); 
+  int tiltAngle = map(objectY, 0, 240, 0, 180);
 
   // Constrain servo angles to valid range
   panAngle = constrain(panAngle, 0, 180);
-  //** tiltAngle = constrain(tiltAngle, 0, 60);
+  tiltAngle = constrain(tiltAngle, 0, 60);
 
   // Set servo angles
   panServo.write(panAngle);
-  //** tiltServo.write(tiltAngle);
-
-  // Move pan servo smoothly to the target angle
-    moveServoSmoothly(panServo, panAngle);
-
-  // Move tilt servo smoothly to the target angle
-  //** moveServoSmoothly(tiltServo, tiltAngle);
-
+  tiltServo.write(tiltAngle);
+  
+  
   // Print object position (for debugging)
   Serial.print("Object Position - X: ");
   Serial.print(objectX);
@@ -105,6 +68,6 @@ void loop() {
   Serial.print("Servo angles - Pan: ");
   Serial.print(panAngle);
   Serial.print(", Tilt: ");
-//**  Serial.println(tiltAngle);
+  Serial.println(tiltAngle);
   }
 }
